a9302cb13bda9a16bf6c37ec51f30578866f1213
AkagiYuu <khanghuynh0245@gmail.com>
2023-01-09 08:33:08 +0700

    Update

--- src/pieces/bundle.rs
+++ src/pieces/bundle.rs
@@ -1,30 +1,13 @@
 use bevy::prelude::{
-    Bundle, Component, ComputedVisibility, GlobalTransform, Handle, Mesh, PbrBundle,
+    Bundle, Component, GlobalTransform, Handle, Mesh, PbrBundle,
     StandardMaterial, Transform, Vec3, Visibility,
 };
+use bevy::prelude::ComputedVisibility;
+use super::components::{PieceKind, PieceSide, Position};
 
-#[derive(Debug, Default, Clone, Copy, Component)]
-pub enum PieceKind {
-    King,
-    Queen,
-    Rook,
-    Knight,
-    Bishop,
-    #[default]
-    Pawn,
-}
-
-#[derive(Debug, Default, Clone, Copy, Component)]
-pub enum PieceSide {
-    #[default]
-    Black,
-    White,
-}
-
-#[derive(Bundle, Clone)]
+#[derive(Debug, Bundle, Clone)]
 pub struct PieceBundle {
-    kind: PieceKind,
-    side: PieceSide,
-    x: f32,
-    y: f32,
+    pub kind: PieceKind,
+    pub side: PieceSide,
+    pub position: Position,
 }
--- src/pieces/mod.rs
+++ src/pieces/mod.rs
@@ -1,11 +1,9 @@
 pub mod bundle;
-pub mod generator;
-pub mod mesh;
+pub mod components;
 
-use bevy::prelude::*;
-use generator::{PieceGenerator, Point};
+use bevy::{prelude::*, utils::HashMap};
 use bundle::*;
-use strum_macros::EnumIter;
+use components::*;
 
 fn bundle(mesh: Handle<Mesh>, material: Handle<StandardMaterial>, position: Vec3) -> PbrBundle {
     PbrBundle {
@@ -16,38 +14,139 @@ fn bundle(mesh: Handle<Mesh>, material: Handle<StandardMaterial>, position: Vec3
     }
 }
 
-macro_rules! bundle {
-    ($mesh:expr, $material:expr, $position:expr) => {
-        (
-            PbrBundle {
-                mesh: $mesh,
-                material: $material,
-                transform: Transform::from_translation($position).with_scale(Vec3::new(0.2, 0.2, 0.2)),
-                ..Default::default()
-            },
-
-        )
-    };
-}
-
 pub fn init(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let material_black = materials.add(Color::rgb(0., 0.2, 0.2).into());
-    let material_white = materials.add(Color::rgb(1., 0.8, 0.8).into());
+    let material = HashMap::from([
+        (
+            PieceSide::Black,
+            materials.add(Color::rgb(0., 0.2, 0.2).into()),
+        ),
+        (
+            PieceSide::White,
+            materials.add(Color::rgb(1., 0.8, 0.8).into()),
+        ),
+    ]);
 
-    let queen_mesh: Handle<Mesh> = asset_server.load("models/pieces.glb#Mesh3/Primitive0");
-    let rook_mesh: Handle<Mesh> = asset_server.load("models/pieces.glb#Mesh4/Primitive0");
-    let bishop_mesh: Handle<Mesh> = asset_server.load("models/pieces.glb#Mesh0/Primitive0");
-    let pawn_mesh: Handle<Mesh> = asset_server.load("models/pieces.glb#Mesh2/Primitive0");
-    let king_mesh: Handle<Mesh> = asset_server.load("models/pieces.glb#Mesh5/Primitive0");
-    let knight_mesh: Handle<Mesh> = asset_server.load("models/pieces.glb#Mesh1/Primitive0");
+    let mesh: HashMap<PieceKind, Handle<Mesh>> = HashMap::from([
+        (
+            PieceKind::King,
+            asset_server.load("models/pieces.glb#Mesh5/Primitive0"),
+        ),
+        (
+            PieceKind::Pawn,
+            asset_server.load("models/pieces.glb#Mesh2/Primitive0"),
+        ),
+        (
+            PieceKind::Queen,
+            asset_server.load("models/pieces.glb#Mesh3/Primitive0"),
+        ),
+        (
+            PieceKind::Rook,
+            asset_server.load("models/pieces.glb#Mesh4/Primitive0"),
+        ),
+        (
+            PieceKind::Bishop,
+            asset_server.load("models/pieces.glb#Mesh0/Primitive0"),
+        ),
+        (
+            PieceKind::Knight,
+            asset_server.load("models/pieces.glb#Mesh1/Primitive0"),
+        ),
+    ]);
 
-    commands.spawn_batch([bundle!(
-        rook_mesh.clone(),
-        material_black.clone(),
-        Vec3::new(0., 0., 0.)
-    )])
+    let bundle =
+        |kind: PieceKind, side: PieceSide, position: Position| -> (PbrBundle, PieceBundle) {
+            (
+                PbrBundle {
+                    mesh: mesh[&kind].clone(),
+                    material: material[&side].clone(),
+                    transform: Transform::from_xyz(position.x, 0., position.y)
+                        .with_scale(Vec3::new(0.2, 0.2, 0.2)),
+                    ..Default::default()
+                },
+                PieceBundle {
+                    kind,
+                    side,
+                    position,
+                },
+            )
+        };
+    commands.spawn_batch([
+        bundle(PieceKind::Rook, PieceSide::Black, Position { x: 0., y: 0. }),
+        bundle(
+            PieceKind::Knight,
+            PieceSide::Black,
+            Position { x: 0., y: 1. },
+        ),
+        bundle(
+            PieceKind::Bishop,
+            PieceSide::Black,
+            Position { x: 0., y: 2. },
+        ),
+        bundle(PieceKind::King, PieceSide::Black, Position { x: 0., y: 3. }),
+        bundle(
+            PieceKind::Queen,
+            PieceSide::Black,
+            Position { x: 0., y: 4. },
+        ),
+        bundle(
+            PieceKind::Bishop,
+            PieceSide::Black,
+            Position { x: 0., y: 5. },
+        ),
+        bundle(
+            PieceKind::Knight,
+            PieceSide::Black,
+            Position { x: 0., y: 6. },
+        ),
+        bundle(PieceKind::Rook, PieceSide::Black, Position { x: 0., y: 7. }),
+    ]);
+    for i in 0..8 {
+        commands.spawn(bundle(
+            PieceKind::Pawn,
+            PieceSide::Black,
+            Position { x: 1., y: i as f32 },
+        ));
+    }
+
+    commands.spawn_batch([
+        bundle(PieceKind::Rook, PieceSide::White, Position { x: 7., y: 0. }),
+        bundle(
+            PieceKind::Knight,
+            PieceSide::White,
+            Position { x: 7., y: 1. },
+        ),
+        bundle(
+            PieceKind::Bishop,
+            PieceSide::White,
+            Position { x: 7., y: 2. },
+        ),
+        bundle(PieceKind::King, PieceSide::White, Position { x: 7., y: 3. }),
+        bundle(
+            PieceKind::Queen,
+            PieceSide::White,
+            Position { x: 7., y: 4. },
+        ),
+        bundle(
+            PieceKind::Bishop,
+            PieceSide::White,
+            Position { x: 7., y: 5. },
+        ),
+        bundle(
+            PieceKind::Knight,
+            PieceSide::White,
+            Position { x: 7., y: 6. },
+        ),
+        bundle(PieceKind::Rook, PieceSide::White, Position { x: 7., y: 7. }),
+    ]);
+    for i in 0..8 {
+        commands.spawn(bundle(
+            PieceKind::Pawn,
+            PieceSide::White,
+            Position { x: 6., y: i as f32 },
+        ));
+    }
 }
